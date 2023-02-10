2c7a7b7b98062fb07dd649a8f39464125a10c36c
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2023-02-11 01:06:55 +0530

    Relatins create done

---
+++ src/enums/relations.ts
@@ -0,0 +1,5 @@
+export const relations={
+    ONETOONE:"One to One",
+    ONETOMANY:"One to Many",
+    MNYTOONE:"Many to One",
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useReducer, useState } from "react";
 import Topbar from "../../components/Topbar";
-import Sidemenu from "../../components/Sidemenu";
+// import Sidemenu from "../../components/Sidemenu";
 import Canvas from "../../components/Canvas";
 import "./styles/index.css";
 import { tableReducer } from "../../reducers/tableReducer";
@@ -14,24 +14,34 @@ function Home() {
         tableReducer,
         [] as Array<TableData>
     );
-    const [relations,relationDispatch]=useReducer(
+    const [relations, relationDispatch] = useReducer(
         relationReducer,
         [] as Array<RelationData>
-    )
-    useEffect(()=>{
-        const initTableData=JSON.parse(localStorage.getItem("tableData")||"{}");
+    );
+    useEffect(() => {
+        const initTableData = JSON.parse(
+            localStorage.getItem("tableData") || "{}"
+        );
         console.log(initTableData);
-        if(initTableData["data"]){
-            console.log("hilo")
-            tableDispatch({type:ACTIONS.SET_DATA,payload:{data:initTableData["data"]}});
+        if (initTableData["data"]) {
+            console.log("hilo");
+            tableDispatch({
+                type: ACTIONS.SET_DATA,
+                payload: { data: initTableData["data"] },
+            });
         }
-    },[])
-    useEffect(()=>{
-        console.log(tables)
-    },[tables])
+    }, []);
+    useEffect(() => {
+        console.log(tables);
+    }, [tables]);
     return (
         <div className=" main min-h-screen text-white">
-            <Topbar tableDispatch={tableDispatch} />
+            <Topbar
+                relations={relations}
+                relationDispatch={relationDispatch}
+                tableDispatch={tableDispatch}
+                tables={tables}
+            />
             {/* <Sidemenu
                 selectedItem={selectedItem}
                 setSelectedItem={setSelectedItem}
