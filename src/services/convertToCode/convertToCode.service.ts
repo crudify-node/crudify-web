5efde57c0309b121b9648542b1216dfb5bb335d0
Abhinandan Sharma <abhi.moudgil15@gmail.com>
2023-03-25 22:35:23 +0530

    Implemented crudify api

--- src/services/convertToCode/convertToCode.service.ts
+++ src/services/convertToCode/convertToCode.service.ts
@@ -1,15 +1,24 @@
 import { type CRUDIFY_DATA } from "../../Constants/CrudifyData";
-
+import { BASE_URL } from "../../envs/environment";
 export const convertToCodeFromCrudifyData = async (
   data: CRUDIFY_DATA
 ): Promise<any> => {
-  console.log({ data });
-  const element = document.createElement("a");
-  const textFile: any = new Blob([JSON.stringify(data) as BlobPart], {
-    type: "json"
+  console.log(data);
+  await fetch(BASE_URL() + "api/crudify", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(async (res) => {
+    const blob = await res.blob();
+    const newBlob = new Blob([blob]);
+    const blobUrl = window.URL.createObjectURL(newBlob);
+    const link = document.createElement("a");
+    link.href = blobUrl;
+    link.setAttribute("download", "target.zip");
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode?.removeChild(link);
   });
-  element.href = URL.createObjectURL(textFile);
-  element.download = "userFile.json";
-  document.body.appendChild(element);
-  element.click();
 };
